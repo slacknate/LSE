2984141c9480ab0c569ded267f910144da15912a
slacknate <slacknate@gmail.com>
2014-07-20 22:46:47 -0400

    updated logger to indent all log messages properly when messages are multiline

--- src/lse/logger.cpp
+++ src/lse/logger.cpp
@@ -15,30 +15,71 @@ const char *const LOG_LEVEL_PREFIXS[] = {
     "R",
 };
 
-void WriteLog(LogLevel log_level, FILE *stream, va_list &arg_list) {
+char* replace_newlines(unsigned int prefix_len, const char *format) {
+    
+    char prefix_buffer[prefix_len];
+    memset(prefix_buffer, ' ', prefix_len);
+    
+    unsigned int format_len = strlen(format);
+    unsigned int num_newlines = 0;
+    for(unsigned int i = 0; i < format_len; ++i) {
+        
+        if(format[i] == '\n')
+            ++num_newlines;
+    }
+    
+    unsigned int new_format_len = (num_newlines * prefix_len) + format_len + 1;
+    char *new_format = new (std::nothrow) char[new_format_len];
+    if(new_format == NULL)
+        ; // uhhh... do we log this? wont that be some SERIOUS RECURSION BRO?!
+    
+    memset(new_format, 0, new_format_len);
+    
+    unsigned int i = 0, k = 0;
+    for(; i < format_len; ++i, ++k) {
+        
+        new_format[k] = format[i];
+        if(format[i] == '\n') {
+            
+            strncat(new_format, prefix_buffer, prefix_len);
+            k += prefix_len;
+        }
+    }
+
+    return new_format;
+}
+
+void write_log(LogLevel log_level, FILE *stream, va_list &arg_list) {
     
     if(log_level <= LOG_LEVEL) {
         
-        char *timeStr = new (std::nothrow) char [9];
-        if(stream != NULL && timeStr != NULL) {
+        char *time_str = new (std::nothrow) char [9];
+        if(stream != NULL && time_str != NULL) {
         
-            memset(timeStr, 0, 9);
+            memset(time_str, 0, 9);
         
             time_t t;
             time(&t);
             char *static_time = ctime(&t);
     
-            strncpy(timeStr, &static_time[11], 8);
+            strncpy(time_str, &static_time[11], 8);
         
-            fprintf(stream, "[%s] %s: ", timeStr, LOG_LEVEL_PREFIXS[log_level]);
-        
-            delete[] timeStr;
-            timeStr = NULL;
+            int prefix_len = fprintf(stream, "[%s] %s: ", time_str, LOG_LEVEL_PREFIXS[log_level]);
+            if(prefix_len < 0) {
+                
+                // FIXME: what should we do here? this is an error case...
+            }
             
             const char *format = va_arg(arg_list, const char *);
-            vfprintf(stream, format, arg_list);
-        
+            char *indented_format = replace_newlines(prefix_len, format);
+            
+            vfprintf(stream, indented_format, arg_list);
             fprintf(stream, "\n");
+            
+            fflush(stream);
+            
+            delete[] time_str;
+            delete[] indented_format;
         }
         else {
         
@@ -52,7 +93,7 @@ void errno_helper(LogLevel log_level, FILE *stream, ...) {
     va_list arg_list;
     va_start(arg_list, stream);
     
-    WriteLog(log_level, stream, arg_list);
+    write_log(log_level, stream, arg_list);
     
     va_end(arg_list);
 }
@@ -65,10 +106,10 @@ void LOG(LogLevel log_level, ...) {
     va_start(arg_list, log_level);
     
     if(log_level == LOG_LEVEL_ERROR)
-        WriteLog(log_level, stderr, arg_list);
+        ::write_log(log_level, stderr, arg_list);
         
     else
-        WriteLog(log_level, stdout, arg_list);
+        ::write_log(log_level, stdout, arg_list);
     
     va_end(arg_list);
 }
