54631f4cf5e8b13ac8c3cd7cbde87cb7a96099de
Nathaniel Duarte <slacknate@gmail.com>
2014-08-17 23:18:20 -0400

    template function definition and declaration must occur simultaneously, as with template classes

--- include/util/memory.h
+++ include/util/memory.h
@@ -1,10 +1,49 @@
 #ifndef LSE_MEMORY_H
 #define LSE_MEMORY_H
 
+#include "lse/defs.h"
+#include "lse/exception.h"
+
 namespace LSE {
 
-template <class T> T* calloc(unsigned int size);
-template <class T> T* malloc(unsigned int size, ...);
+/*
+Allocate a block of memory and initialze to zero.
+Throw an exception in the event of allocation failure.
+This should be used for basic types and structs.
+*/
+template <class T> T* calloc(unsigned int size) {
+    
+    T *t_ptr = new (std::nothrow) T [size];
+    if(t_ptr != nullptr) {
+        
+        memset(t_ptr, 0, sizeof(T) * size);
+    }
+    else{
+        
+        throw Exception("NOT_REAL", 0, BAD_ALLOC); // FIXME: get real file and line
+    }
+    
+    return t_ptr;
+}
+
+
+/*
+Allocate a block of memory.
+Throw an exception in the event of allocation failure.
+This should be used for classes. Note that this will only
+call a default constructor; if a more specialized constructor
+is required, use template specialization to override this function.
+*/
+template <class T> T* malloc(unsigned int size, ...) {
+    
+    T *t_ptr = new (std::nothrow) T [size];
+    if(t_ptr == nullptr) {
+        
+        throw Exception("NOT_REAL", 0, BAD_ALLOC); // FIXME: get real file and line
+    }
+    
+    return t_ptr;
+}
 
 }
 
