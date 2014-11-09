49a2ca0288fd953fb44f723a31af3ba670542c5d
Nathaniel Duarte <slacknate@gmail.com>
2014-11-08 23:11:43 -0500

    update the platform specific include files to be compatible for the cmake platform detection

--- include/gui/window/window.h
+++ include/gui/window/window.h
@@ -1,17 +1,21 @@
 #ifndef LSE_GL_WINDOW_H
 #define LSE_GL_WINDOW_H
 
-#if defined(_WIN32) || defined(_WIN64) // windows
+#if WIN32 // windows
 
 #include "gui/window/win.h"
 
-#elif defined(__unix__) && !defined(__APPLE__) // all unix variants, non-mac
+#elif UNIX // all unix variants, non-mac
 
 #include "gui/window/nix.h"
 
-#elif defined(__APPLE__) && !defined(__MACH__) // mac osx, non-ios
+#elif APPLE // mac osx
 
 #include "gui/window/mac.h"
+
+#else
+
+#warning NO PLATFORM DETECTED
 
 #endif
 
--- include/io/handler.h
+++ include/io/handler.h
@@ -1,17 +1,21 @@
 #ifndef LSE_IO_HANDLER_H
 #define LSE_IO_HANDLER_H
 
-#if defined(_WIN32) || defined(_WIN64) // windows
+#if(WIN32) // windows
 
 #include "io/win.h"
 
-#elif defined(__unix__) && !defined(__APPLE__) // all unix variants, non-mac
+#elif(UNIX) // all unix variants, non-mac
 
 #include "io/nix.h"
 
-#elif defined(__APPLE__) && !defined(__MACH__) // mac osx, non-ios
+#elif(APPLE) // mac osx
 
 #include "io/mac.h"
+
+#else
+
+#warning NO PLATFORM DETECTED
 
 #endif
 
