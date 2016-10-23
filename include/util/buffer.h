61b96c601f95feeeb590ab7af017e62973c2fda2
Nate Duarte <slacknate@gmail.co>
2016-10-23 01:58:35 -0400

    include/util/buffer.h: overhaul of the buffer; the logic here was pretty incorrect
     - initialize the buffer with all zeros so we are at a known NULL state
     - add semaphore to control read and write sync
        - previously only one semaphore was present, however this was not enough
        - writes need to know when a spot in the buffer is freed up when it wraps back around
        - reads need to know when a spot in the buffer is populated when it wraps back around
     - rename some variables to be more concise
     - multiline comments now actually follow the standard practice

--- include/util/buffer.h
+++ include/util/buffer.h
@@ -8,17 +8,18 @@
 namespace LSE {
 
 /*
-Generic thread-safe ring buffer template class for
-writing a buffer for any sort of data type!
-*/
+ * Generic thread-safe ring buffer template class for
+ * writing a buffer for any sort of data type!
+ */
 template <class T> class Buffer {
     
     private:
         
-        Mutex read_pos_lock; // lock for buffer read position
-        Mutex write_pos_lock; // lock for buffer write position
-        Semaphore read_write_sync; // semaphore to ensure the buffer cannot overrite itself
-        
+        Mutex read_lock; // lock for buffer read position
+        Mutex write_lock; // lock for buffer write position
+        Semaphore read_sem; // semaphore to ensure the buffer read past existing data
+        Semaphore write_sem; // semaphore to ensure the buffer cannot overwrite itself
+
         const unsigned int size; // size of the buffer
         
         unsigned int _read_pos; // actual read position
@@ -27,56 +28,49 @@ template <class T> class Buffer {
         T **buffer; // the actual buffer memory!
         
         /*
-        Get the current buffer read position.
-        */
-        unsigned int read_pos() {
-            
-            read_pos_lock.lock();
-            
-            unsigned int pos = this->_read_pos;
-            
+         * Modularly increment the read pointer based on buffer size.
+         */
+        void inc_read_pos() {
+
             ++this->_read_pos;
             if(this->_read_pos >= this->size)
                 this->_read_pos = 0;
-            
-            read_pos_lock.unlock();
-            
-            return pos;   
         }
         
         /*
-        Get the current buffer write position.
-        */
-        unsigned int write_pos() {
-            
-            write_pos_lock.lock();
-            
-            unsigned int pos = this->_write_pos;
+         * Modularly increment the write pointer based on buffer size.
+         */
+        void inc_write_pos() {
             
             ++this->_write_pos;
             if(this->_write_pos >= this->size)
                 this->_write_pos = 0;
-            
-            write_pos_lock.unlock();
-            
-            return pos;   
         }
         
     public:
         
         /*
-        Initialze the buffer size, buffer memory, and semaphore initial value.
-        */
-        Buffer(unsigned int _size) : read_write_sync(_size), size(_size), buffer(new T *[_size]) {
+         * Initialize the buffer size and buffer memory.
+         */
+        Buffer(unsigned int _size) : size(_size), buffer(new T *[_size]) {
             
+            /*
+             * Initially read and write pointer both
+             * point to our first item in the buffer.
+             */
             this->_read_pos = 0;
             this->_write_pos = 0;
+
+            /*
+             * Initialize all our buffer items to nullptr.
+             */
+            memset(buffer, 0, _size);
         }
         
         /*
-        Delete all items currently in the buffer, and clean up the
-        remaining buffer memory.
-        */
+         * Delete all items currently in the buffer, and clean up the
+         * remaining buffer memory.
+         */
         ~Buffer() {
             
             this->flush();
@@ -85,52 +79,81 @@ template <class T> class Buffer {
         }
         
         /*
-        Delete all items currently in the buffer.
-        */
+         * Delete all items currently in the buffer.
+         */
         void flush() {
-            
-            read_pos_lock.lock();
-            write_pos_lock.lock();
+
+            read_lock.lock();
+            write_lock.lock();
             
             for(unsigned int i = 0; i < this->size; ++i) {
                 
                 if(this->buffer[i] != nullptr) {
                     
                     delete this->buffer[i];
+                    this->buffer[i] = nullptr;
                 }
             }
-            
-            write_pos_lock.unlock();
-            read_pos_lock.unlock();
+
+            write_lock.unlock();
+            read_lock.unlock();
         }
         
         /*
-        Write to the buffer.
-        */
-        void push(const T &t) {
-            
-            read_write_sync.wait();
-            
-            unsigned int pos = this->write_pos();
-            this->buffer[pos] = new (std::nothrow) T(t);
-            
-            if(this->buffer[pos] == nullptr) {
-                
-                // raise exception here
-            }
+         * Write to the buffer.
+         */
+        void push(T *const t) {
+
+            write_lock.lock();
+
+            unsigned int pos = this->_write_pos;
+            this->inc_write_pos();
+
+            write_lock.unlock();
+
+            /*
+             * If there is valid data in this location in
+             * the buffer then we need to wait until the
+             * reading caught up before we can write.
+             */
+            if(this->buffer[pos] != nullptr)
+                this->write_sem.wait();
+
+            this->buffer[pos] = t;
+
+            /*
+             * Post to the read semaphore when we push an
+             * item into the buffer so we know we have
+             * data available.
+             */
+            this->read_sem.post();
         }
         
         /*
-        Read from the buffer.
-        */
-        T* pop() {
-            
-            unsigned int pos = this->read_pos();
-            T *t = this->buffer[pos];
+         * Read from the buffer.
+         */
+        T *const pop() {
+
+            read_lock.lock();
+
+            unsigned int pos = this->_read_pos;
+            this->inc_read_pos();
+
+            read_lock.unlock();
+
+            if(this->buffer[pos] == nullptr)
+                this->read_sem.wait();
+
+            T *const t = this->buffer[pos];
             this->buffer[pos] = nullptr;
-            
-            read_write_sync.post();
-            
+
+            /*
+             * Post to the write semaphore when we pop an
+             * item out of the buffer so we know we have
+             * an open spot.
+             */
+            write_sem.post();
+
             return t;
         }        
 };
