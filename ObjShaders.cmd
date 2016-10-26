61d03e6c7dec6a93b1f2aa3b83b0a6facf4de17c
Nate Duarte <slacknate@gmail.co>
2016-10-25 21:43:39 -0400

    [lse] remove unused shader dump script

--- ObjShaders.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-shaders\dump\bin\ShaderDump.exe C:\Users\Nate\Documents\Dev-Cpp\OpenGL\platformer\shaders ObjShaders C:\Users\Nate\Documents\Dev-Cpp\OpenGL\platformer\src C:\Users\Nate\Documents\Dev-Cpp\OpenGL\platformer\include 
