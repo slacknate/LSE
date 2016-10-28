5293408e76d317269e777a33642d344c63a4ff5f
Nate Duarte <slacknate@gmail.co>
2016-10-27 20:00:34 -0400

    [matrix] flesh out the matrix class a little more so we might actually be able to start using it
     - implement smart [] indexing
     - implement new ctor to create square/identity matrices

--- include/util/math/matrix.h
+++ include/util/math/matrix.h
@@ -7,18 +7,39 @@
 namespace LSE {
 
 /*
+ * Object to implement smart double indexing into our matrix.
+ */
+class MatrixColumn {
+
+    private:
+
+        unsigned int rows;
+        float *column;
+
+    public:
+
+        MatrixColumn(unsigned int _rows);
+
+        void set_column(float *col_ptr);
+
+        float* operator[](const int& row);
+};
+
+/*
 A matrix stored in column major format.
 */
 class Matrix {
     
     private:
-        
-        unsigned int rows, columns; // matrix dimensions
+
+        MatrixColumn mat_col; // object to return a reference to in in operator[]
+        unsigned int columns, rows; // matrix dimensions
         float *matrix; // matrix data
         
     public:
         
-        Matrix(unsigned int r, unsigned int c);
+        Matrix(unsigned int _columns, unsigned int _rows);
+        Matrix(unsigned int _col_rows, bool identity=false);
         Matrix(const Matrix& other);
         ~Matrix();
         
@@ -30,7 +51,9 @@ class Matrix {
         Matrix bot_triangular();
         
         float determinant();
-        
+
+        MatrixColumn& operator[](const int& column);
+
         void operator*(const float& scalar);
         void operator/(const float& scalar);
         Matrix operator+(const Matrix& other);
--- src/util/math/matrix.cpp
+++ src/util/math/matrix.cpp
@@ -1,23 +1,73 @@
 #include <cstring>
 #include "lse/exception.h"
+#include "util/memory.h"
 #include "util/math/matrix.h"
 using namespace LSE;
+
+
+/*
+ * Initialize our number of rows from our matrix
+ * and set our column pointer to NULL.
+ */
+MatrixColumn::MatrixColumn(unsigned int _rows) : rows(_rows), column(nullptr) {}
+
+
+/*
+ * Set our current column pointer.
+ */
+void MatrixColumn::set_column(float *col_ptr) {
+
+    this->column = col_ptr;
+}
+
+/*
+ * Return a pointer to the specified row of our column pointer.
+ *
+ * This is our second dimension indexing into our matrix.
+ *
+ * If the specified row is greater than the number of rows we
+ * throw an exception so we do not do nasty stuff to memory!
+ */
+float* MatrixColumn::operator[](const int& row) {
+
+    if(row >= this->rows)
+        throw EXCEPTION("Matrix only has %u rows, cannot index into row %u.", this->rows, row);
+
+    return &this->column[row];
+}
+
 
 /*
  * Initialize our matrix in memory and zero it out.
  */
-Matrix::Matrix(unsigned int _rows, unsigned int _columns) :
-        rows(_rows), columns(_columns), matrix(new float [_rows * _columns]) {
+Matrix::Matrix(unsigned int _columns, unsigned int _rows) : columns(_columns), rows(_rows), mat_col(_rows) {
 
-    memset(this->matrix, 0, _rows * _columns);
+    this->matrix = LSE::CALLOC(float, _columns * _rows);
+}
+
+/*
+ * Create a square matrix, optionally initializing
+ * it to be an identity matrix.
+ */
+Matrix::Matrix(unsigned int _cols_rows, bool identity) : Matrix(_cols_rows, _cols_rows) {
+
+    if(identity) {
+
+        for(unsigned int i = 0; i < _cols_rows; ++i)
+            this->matrix[i + (i * _cols_rows)] = 1.0f;
+    }
 }
 
 /*
  * Create a copy of a pre-existing matrix.
  */
-Matrix::Matrix(const Matrix& other) : Matrix(other.rows, other.columns) {}
+Matrix::Matrix(const Matrix& other) : Matrix(other.rows, other.columns) {
+
+    memcpy(this->matrix, other.matrix, sizeof(float) * other.rows * other.columns);
+}
 
 /*
+ * Free up the array created to represent this matrix.
  */
 Matrix::~Matrix() {
     
@@ -34,6 +84,7 @@ float* Matrix::get_matrix() {
 }
  
 /*
+ * FIXME: implement!
  */
 Matrix Matrix::inverse() {
 
@@ -41,6 +92,7 @@ Matrix Matrix::inverse() {
 }
 
 /*
+ * FIXME: implement!
  */
 Matrix Matrix::transpose() {
 
@@ -48,6 +100,7 @@ Matrix Matrix::transpose() {
 }
 
 /*
+ * FIXME: implement!
  */
 Matrix Matrix::top_triangular() {
     
@@ -55,6 +108,7 @@ Matrix Matrix::top_triangular() {
 }
 
 /*
+ * FIXME: implement!
  */
 Matrix Matrix::bot_triangular() {
     
@@ -62,6 +116,7 @@ Matrix Matrix::bot_triangular() {
 }
 
 /*
+ * FIXME: implement!
  */
 float Matrix::determinant() {
     
@@ -79,6 +134,29 @@ float Matrix::determinant() {
 }
 
 /*
+ * Set the current column in our MatrixColumn object
+ * and then return a reference to it.
+ *
+ * If the specified column is greater than the number of columns we
+ * throw an exception so we do not do nasty stuff to memory!
+ *
+ * This allows us to bounds check on both the column
+ * and the row index so we can throw an exception if
+ * necessary.
+ */
+MatrixColumn& Matrix::operator[](const int& column) {
+
+    if(column >= this->columns)
+        throw EXCEPTION("Matrix only has %u rows, cannot index into row %u.", this->columns, column);
+
+    float *col_ptr = &this->matrix[column * this->rows];
+    this->mat_col.set_column(col_ptr);
+
+    return this->mat_col;
+}
+
+/*
+ * Matrix scalar multiplication.
  */
 void Matrix::operator*(const float& scalar) {
     
@@ -87,6 +165,7 @@ void Matrix::operator*(const float& scalar) {
 }
 
 /*
+ * Matrix scalar division.
  */
 void Matrix::operator/(const float& scalar) {
 
@@ -95,6 +174,7 @@ void Matrix::operator/(const float& scalar) {
 }
 
 /*
+ * Add a matrix to this matrix.
  */
 Matrix Matrix::operator+(const Matrix& other) {
     
@@ -114,6 +194,7 @@ Matrix Matrix::operator+(const Matrix& other) {
 }
 
 /*
+ * Subtract a matrix from this matrix.
  */
 Matrix Matrix::operator-(const Matrix& other) {
 
@@ -133,6 +214,7 @@ Matrix Matrix::operator-(const Matrix& other) {
 }
 
 /*
+ * FIXME: implement!
  */
 Matrix Matrix::operator*(const Matrix& other) {
     
@@ -157,7 +239,7 @@ Matrix Matrix::operator*(const Matrix& other) {
 }
  
 /*
- * "Divide" this matrix by another. We define matrix divison "A/B" as "A(B^-1)"
+ * "Divide" this matrix by another. We define matrix division "A/B" as "A(B^-1)"
  */
 Matrix Matrix::operator/(Matrix& other) {
     
@@ -165,16 +247,37 @@ Matrix Matrix::operator/(Matrix& other) {
 }
 
 /*
- 
-*/
+ * Check for equality of two matrices.
+ *
+ * If they are unequal in size then they are
+ * automatically considered not equal.
+ *
+ * Otherwise we compare the data in memory for equality.
+ */
 bool Matrix::operator==(const Matrix& other) {
-    
 
+    bool result = false;
+
+    if(this->rows == other.rows && this->columns == other.columns)
+        result = (memcmp(this->matrix, other.matrix, sizeof(float) * other.rows * other.columns) == 0);
+
+    return result;
 }
 
 /*
+ * Check for inequality of two matrices.
+ *
+ * If they are unequal in size then they are
+ * automatically considered not equal.
+ *
+ * Otherwise we compare the data in memory for inequality.
  */
 bool Matrix::operator!=(const Matrix& other) {
 
+    bool result = true;
 
+    if(this->rows == other.rows && this->columns == other.columns)
+        result = (memcmp(this->matrix, other.matrix, sizeof(float) * other.rows * other.columns) != 0);
+
+    return result;
 }
